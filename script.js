27045a036c647682566c4d983217d009f3ef422a
James Craine <jamesdcraine@gmail.com>
2020-01-08 16:47:09 -0500

    Added needed stylesheets and links, stepped through logic needed in jsfile

---
+++ script.js
@@ -0,0 +1,17 @@
+//set up local storage
+
+//pull up document
+
+//create 1 row
+
+//create column 1
+
+//create column 2
+
+//create column 3
+
+//add columns to row
+
+//save button
+
+//somehow updating colors based on current time
