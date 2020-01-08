0e053ea02a558bae95f0ccd2ed747aaf44762b8f
James Craine <jamesdcraine@gmail.com>
2020-01-08 18:15:47 -0500

    started row and column script, messed with jumbotron

--- script.js
+++ script.js
@@ -1,20 +1,33 @@
 //set up local storage
-
+function getLocalStorage(key) {
+    let value = localStorage.getItem(key);
+    if (value) {
+        $(`#text${key}`).text(value);
+    }
+}
 //pull up document
+$(document).ready(function() {
+    $("#currentDay").text(moment().format("dddd, MMMM Do" + ", 2020"));
+    for (let i = 8; i < 24; i++) {
 
 //create 1 row
-var row = $
+var row = $ (<div data-time);
 
 //create column 1
-var column1 = $
+var column1 = $(<div class="col-sm-4">);
 
 //create column 2
-var column2 = $
+var column2 = $(<div class="col-sm-4">);
 
 //create column 3
-var column3 = $
+var column3 = $(<div class="col-sm-4">);
 
-//add columns to row
+// add columns to row
+row.append(column1);
+row.append(column2);
+row.append(column3);
+
+$(".container").append(row);
 
 //save button
 
