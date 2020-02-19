875a3e5f620f58dc0fb89eab8049075baad4c204
James Craine <jamesdcraine@gmail.com>
2020-02-19 14:44:16 -0500

    set up moment and attempted to set up local storage

--- script.js
+++ script.js
@@ -1,11 +1,24 @@
+// putting moment in a variable
 var today = moment();
 
+// putting the current date in a variable
 var currentDay = moment().format("dddd, MMMM " + "Do, YYYY " + "-- h:mm a");
 
+// setting the date
 function insertCurrentDay() {
     $("#moment").text(currentDay);
 };
 
 insertCurrentDay();
 
+// setting up local storage
 
+saveActivity.addEventListener('saveBtn', function (event) {
+	if (saveActivity.value.length < 1) return;
+	localStorage.setItem('saveActivity', saveActivity.innerHTML);
+}, false);
+
+var saved = localStorage.getItem('saveActivity');
+if (saved) {
+	saveActivity.innerHTML = saved;
+}
