5631e7607163c56a368c6ddbcf9026d6ca5522e5
James Craine <jamesdcraine@gmail.com>
2020-02-19 16:18:55 -0500

    got local storage working

--- script.js
+++ script.js
@@ -13,12 +13,50 @@ insertCurrentDay();
 
 // setting up local storage
 
-saveActivity.addEventListener('saveBtn', function (event) {
-	if (saveActivity.value.length < 1) return;
-	localStorage.setItem('saveActivity', saveActivity.innerHTML);
-}, false);
-
-var saved = localStorage.getItem('saveActivity');
-if (saved) {
-	saveActivity.innerHTML = saved;
+function textNine(){
+	var dataToSaveNine = document.getElementById("inputNine").value;	
+	localStorage.setItem('data', dataToSaveNine);
+	console.log(dataToSaveNine);
 }
+function textTen(){
+	var dataToSaveTen = document.getElementById("inputTen").value;
+	localStorage.setItem("data", dataToSaveTen);
+
+}
+function textEleven(){
+	var dataToSaveEleven = document.getElementById("inputEleven").value;
+	localStorage.setItem("data", dataToSaveEleven);
+
+}
+function textTwelve(){
+	var dataToSaveTwelve = document.getElementById("inputTwelve").value;
+	localStorage.setItem("data", dataToSaveTwelve);
+
+}
+function textOne(){
+	var dataToSaveOne = document.getElementById("inputOne").value;
+	localStorage.setItem("data", dataToSaveOne);
+
+}
+function textTwo(){
+	var dataToSaveTwo = document.getElementById("inputTwo").value;
+	localStorage.setItem("data", dataToSaveTwo);
+
+}
+function textThree(){
+	var dataToSaveThree = document.getElementById("inputThree").value;
+	localStorage.setItem("data", dataToSaveThree);
+
+}
+function textFour(){
+	var dataToSaveFour = document.getElementById("inputFour").value;
+	localStorage.setItem("data", dataToSaveFour);
+	
+}
+function textFive(){
+	var dataToSaveFive = document.getElementById("inputFive").value;
+	localStorage.setItem("data", dataToSaveFive);
+	alert(dataToSaveFive);
+}
+
+
