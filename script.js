56e5785169db6190a1260dd1dd25a676dc989803
James Craine <jamesdcraine@gmail.com>
2020-02-21 15:41:44 -0500

    cleaned up a couple things, tried to simplify a little

--- script.js
+++ script.js
@@ -16,56 +16,38 @@ insertCurrentDay();
 function textNine(){
 	var dataToSaveNine = document.getElementById("inputNine").value;	
 	localStorage.setItem("inputNine", dataToSaveNine);
-	window.localStorage.getItem(dataToSaveNine);
-	JSON.parse(window.localStorage.getItem(dataToSaveNine));
 }
 function textTen(){
 	var dataToSaveTen = document.getElementById("inputTen").value;
 	localStorage.setItem("inputTen", dataToSaveTen);
-	window.localStorage.getItem(dataToSaveTen);
-	JSON.parse(window.localStorage.getItem(dataToSaveTen));
 }
 function textEleven(){
 	var dataToSaveEleven = document.getElementById("inputEleven").value;
 	localStorage.setItem("inputEleven", dataToSaveEleven);
-	window.localStorage.getItem(dataToSaveEleven);
-	JSON.parse(window.localStorage.getItem(dataToSaveEleven));
 }
 function textTwelve(){
 	var dataToSaveTwelve = document.getElementById("inputTwelve").value;
 	localStorage.setItem("inputTwelve", dataToSaveTwelve);
-	window.localStorage.getItem(dataToSaveTwelve);
-	JSON.parse(window.localStorage.getItem(dataToSaveTwelve));
 }
 function textOne(){
 	var dataToSaveOne = document.getElementById("inputOne").value;
 	localStorage.setItem("inputOne", dataToSaveOne);
-	window.localStorage.getItem(dataToSaveOne);
-	JSON.parse(window.localStorage.getItem(dataToSaveOne));
 }
 function textTwo(){
 	var dataToSaveTwo = document.getElementById("inputTwo").value;
 	localStorage.setItem("inputTwo", dataToSaveTwo);
-	window.localStorage.getItem(dataToSaveTwo);
-	JSON.parse(window.localStorage.getItem(dataToSaveTwo));
 }
 function textThree(){
 	var dataToSaveThree = document.getElementById("inputThree").value;
 	localStorage.setItem("inputThree", dataToSaveThree);
-	window.localStorage.getItem(dataToSaveThree);
-	JSON.parse(window.localStorage.getItem(dataToSaveThree));
 }
 function textFour(){
 	var dataToSaveFour = document.getElementById("inputFour").value;
 	localStorage.setItem("inputFour", dataToSaveFour);
-	window.localStorage.getItem(dataToSaveFour);
-	JSON.parse(window.localStorage.getItem(dataToSaveFour));
 }
 function textFive(){
 	var dataToSaveFive = document.getElementById("inputFive").value;
 	localStorage.setItem("inputFive", dataToSaveFive);
-	window.localStorage.getItem(dataToSaveFive);
-	JSON.parse(window.localStorage.getItem(dataToSaveFive));
 }
 
 textNine();
@@ -161,19 +143,3 @@ else if (currentHour == 17) {
 else if (currentHour < 17) {
 	document.getElementById("fivePM").style.borderColor = "green";
 }
-
-// document.getElementById("result").innerHTML = localStorage.getItem("inputNine");
-// document.getElementById("result").innerHTML = localStorage.getItem("inputTen");
-// document.getElementById("result").innerHTML = localStorage.getItem("inputEleven");
-// document.getElementById("result").innerHTML = localStorage.getItem("inputTwelve");
-// document.getElementById("result").innerHTML = localStorage.getItem("inputOne");
-// document.getElementById("result").innerHTML = localStorage.getItem("inputTwo");
-// document.getElementById("result").innerHTML = localStorage.getItem("inputThree");
-// document.getElementById("result").innerHTML = localStorage.getItem("inputFour");
-// document.getElementById("result").innerHTML = localStorage.getItem("inputFive");
-
-
-
-
-
-
