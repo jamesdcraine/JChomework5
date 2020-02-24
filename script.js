346d458d1f43248a9f2645f09c7aaf41b8a83b16
James Craine <jamesdcraine@gmail.com>
2020-02-24 14:04:19 -0500

    local storage

--- script.js
+++ script.js
@@ -17,47 +17,57 @@ insertCurrentDay();
 function textNine(){
 	var dataToSaveNine = document.getElementById("inputNine").value;	
 	localStorage.setItem("data", dataToSaveNine);  
-	localStorage.getItem("data", dataToSaveNine)
+	localStorage.getItem("data", dataToSaveNine);
 	console.log(dataToSaveNine)}
 
 function textTen(){
 	var dataToSaveTen = document.getElementById("inputTen").value;
 	localStorage.setItem("data", dataToSaveTen);
+	localStorage.getItem("data", dataToSaveTen);
+	console.log(dataToSaveTen);
 }
 function textEleven(){
 	var dataToSaveEleven = document.getElementById("inputEleven").value;
 	localStorage.setItem("data", dataToSaveEleven);
+	localStorage.getItem("data", dataToSaveEleven);
+	console.log(dataToSaveEleven);
 }
 function textTwelve(){
 	var dataToSaveTwelve = document.getElementById("inputTwelve").value;
 	localStorage.setItem("data", dataToSaveTwelve);
+	localStorage.getItem("data", dataToSaveTwelve);
+	console.log(dataToSaveTwelve);
 }
 function textOne(){
 	var dataToSaveOne = document.getElementById("inputOne").value;
 	localStorage.setItem("data", dataToSaveOne);
+	localStorage.getItem("data", dataToSaveOne);
+	console.log(dataToSaveOne);
 }
 function textTwo(){
 	var dataToSaveTwo = document.getElementById("inputTwo").value;
 	localStorage.setItem("data", dataToSaveTwo);
+	localStorage.getItem("data", dataToSaveTwo);
+	console.log(dataToSaveTwo);
 }
 function textThree(){
 	var dataToSaveThree = document.getElementById("inputThree").value;
 	localStorage.setItem("data", dataToSaveThree);
+	localStorage.getItem("data", dataToSaveThree);
+	console.log(dataToSaveThree);
 }
 function textFour(){
 	var dataToSaveFour = document.getElementById("inputFour").value;
 	localStorage.setItem("data", dataToSaveFour);
+	localStorage.getItem("data", dataToSaveFour);
+	console.log(dataToSaveFour);
 }
 function textFive(){
 	var dataToSaveFive = document.getElementById("inputFive").value;
 	localStorage.setItem("data", dataToSaveFive);
-	localStorage.getItem(dataToSaveFive);
+	localStorage.getItem("data", dataToSaveFive);
 	console.log(dataToSaveFive);
 }
-
-function updateStorage(){
-    $("#inputNine").html(localStorage.dataToSaveNine);
- }
 
 textNine();
 textTen();
