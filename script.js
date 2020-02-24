b4d2b14eb5f5b69ec41351ca0a19d8f484811dac
James Craine <jamesdcraine@gmail.com>
2020-02-24 13:18:05 -0500

    local storage

--- script.js
+++ script.js
@@ -15,40 +15,40 @@ insertCurrentDay();
 // setting up local storage
 function textNine(){
 	var dataToSaveNine = document.getElementById("inputNine").value;	
-	localStorage.setItem("inputNine", dataToSaveNine);  
+	localStorage.setItem("data", dataToSaveNine);  
 	console.log(dataToSaveNine)
 }
 function textTen(){
 	var dataToSaveTen = document.getElementById("inputTen").value;
-	localStorage.setItem("inputTen", dataToSaveTen);
+	localStorage.setItem("data", dataToSaveTen);
 }
 function textEleven(){
 	var dataToSaveEleven = document.getElementById("inputEleven").value;
-	localStorage.setItem("inputEleven", dataToSaveEleven);
+	localStorage.setItem("data", dataToSaveEleven);
 }
 function textTwelve(){
 	var dataToSaveTwelve = document.getElementById("inputTwelve").value;
-	localStorage.setItem("inputTwelve", dataToSaveTwelve);
+	localStorage.setItem("data", dataToSaveTwelve);
 }
 function textOne(){
 	var dataToSaveOne = document.getElementById("inputOne").value;
-	localStorage.setItem("inputOne", dataToSaveOne);
+	localStorage.setItem("data", dataToSaveOne);
 }
 function textTwo(){
 	var dataToSaveTwo = document.getElementById("inputTwo").value;
-	localStorage.setItem("inputTwo", dataToSaveTwo);
+	localStorage.setItem("data", dataToSaveTwo);
 }
 function textThree(){
 	var dataToSaveThree = document.getElementById("inputThree").value;
-	localStorage.setItem("inputThree", dataToSaveThree);
+	localStorage.setItem("data", dataToSaveThree);
 }
 function textFour(){
 	var dataToSaveFour = document.getElementById("inputFour").value;
-	localStorage.setItem("inputFour", dataToSaveFour);
+	localStorage.setItem("data", dataToSaveFour);
 }
 function textFive(){
 	var dataToSaveFive = document.getElementById("inputFive").value;
-	localStorage.setItem("inputFive", dataToSaveFive);
+	localStorage.setItem("data", dataToSaveFive);
 	localStorage.getItem(dataToSaveFive);
 	console.log(dataToSaveFive);
 }
